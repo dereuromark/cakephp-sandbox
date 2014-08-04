815d27d34145853f34113feca58446944898ce85
euromark <euromark@web.de>
2014-08-04 11:08:13 +0200

    Add markdown

---
+++ Plugin/Sandbox/Controller/MarkupController.php
@@ -0,0 +1,29 @@
+<?php
+App::uses('SandboxAppController', 'Sandbox.Controller');
+
+class MarkupController extends SandboxAppController {
+
+	public function beforeFilter() {
+		parent::beforeFilter();
+
+		$this->Auth->allow();
+	}
+
+	public function index() {
+		$actions = $this->_getActions($this);
+
+		$this->set(compact('actions'));
+	}
+
+	/**
+	 * TryoutsController::fontawesome()
+	 *
+	 * @see http://fontawesome.io/
+	 *
+	 * @return void
+	 */
+	public function markdown() {
+	}
+
+}
+
