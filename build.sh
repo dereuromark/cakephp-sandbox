040da1e746852d548d34ef0ca2feca67207d42fb
Mark Scherer <euromark@web.de>
2015-02-03 22:02:46 +0100

    Add note

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# Warning: This is NOT a productive script, but for local dev envs only!
 
 echo "### INSTALL/UPDATE ###";
 php composer.phar selfupdate
 
 git pull
 
-php composer.phar update --prefer-dist --no-dev --optimize-autoloader
+php composer.phar update --prefer-dist --no-dev --optimize-autoloader --no-interaction
 
 echo "### DB MIGRATION ###";
 # Don't forget to "chmod +x bin/cake" first
