6205e2d7c56550510b7e2c2c721b53395308dc9f
Mark Scherer <euromark@web.de>
2015-02-11 14:14:28 +0100

    sql

---
+++ config/Migrations/20150121000256_slug.php
@@ -0,0 +1,35 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Slug extends AbstractMigration {
+
+	/**
+	 * Migrate Up.
+	 */
+	public function up() {
+		$content = <<<SQL
+CREATE TABLE IF NOT EXISTS `sandbox_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `username` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
+  `role_id` tinyint(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+);
+SQL;
+
+		$this->query($content);
+	}
+
+	/**
+	 * Migrate Down.
+	 */
+	public function down() {
+
+	}
+
+}
