36e55e472aeb04aa348dfdcb8f3d7e0ccdfc0868
Mark Scherer <euromark@web.de>
2015-03-28 19:44:27 +0100

    Add bootstrap examples

---
+++ src/Plugin/Sandbox/src/Controller/BootstrapController.php
@@ -0,0 +1,33 @@
+<?php
+namespace Sandbox\Controller;
+
+use Cake\Event\Event;
+
+class BootstrapController extends SandboxAppController {
+
+	public $modelClass = 'Sandbox.Animals';
+
+	public $components = ['Search.Prg'];
+
+	public $helpers = ['Form' => ['className' => 'BootstrapUI.Form'], 'Flash' => ['className' => 'BootstrapUI.Flash']];
+
+	public function initialize() {
+		parent::initialize();
+	}
+
+	public function beforeFilter(Event $event) {
+		unset($this->helpers['Tools.Form']);
+		unset($this->helpers['Tools.Flash']);
+
+		$this->Auth->allow();
+		parent::beforeFilter($event);
+	}
+
+	public function index() {
+		$animal = $this->Animals->newEntity();
+
+
+		$this->set(compact('animal'));
+	}
+
+}
