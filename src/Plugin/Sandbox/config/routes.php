e55981ae2528607d9426d8bc237471fa073063bc
Mark Scherer <euromark@web.de>
2015-03-28 20:18:21 +0100

    improve routing

--- src/Plugin/Sandbox/config/routes.php
+++ src/Plugin/Sandbox/config/routes.php
@@ -5,13 +5,15 @@ use Cake\Routing\Router;
 
 
 Router::prefix('admin', function ($routes) {
-	$routes->plugin('Sandbox', function ($routes) {
-		$routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'DashedRoute']);
-		$routes->connect('/:controller/:action/*', [], ['routeClass' => 'DashedRoute']);
-	});
+		$routes->plugin('Sandbox', function ($routes) {
+			$routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'DashedRoute']);
+			$routes->connect('/:controller/:action/*', [], ['routeClass' => 'DashedRoute']);
+		});
 });
 
 Router::plugin('Sandbox', function ($routes) {
-			$routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'DashedRoute']);
-			$routes->connect('/:controller/:action/*', [], ['routeClass' => 'DashedRoute']);
+		$routes->connect('/', ['controller' => 'Sandbox', 'action' => 'index'], ['routeClass' => 'DashedRoute']);
+
+		$routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'DashedRoute']);
+		$routes->connect('/:controller/:action/*', [], ['routeClass' => 'DashedRoute']);
 });
