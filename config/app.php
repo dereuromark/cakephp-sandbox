00c3ee379dc687bfdd62b80292146f47c435a79f
Mark Scherer <euromark@web.de>
2015-04-20 20:17:05 +0200

    Simplify plugins namespace

--- config/app.php
+++ config/app.php
@@ -47,7 +47,7 @@ return [
 		'cssBaseUrl' => 'css/',
 		'jsBaseUrl' => 'js/',
 		'paths' => [
-			'plugins' => [ROOT . DS . 'plugins' . DS, APP . 'Plugin' . DS],
+			'plugins' => [ROOT . DS . 'plugins' . DS],
 			'templates' => [APP . 'Template' . DS],
 			'locales' => [APP . 'Locale' . DS],
 		],
