a2e02054599b1af525940ff3b1a09cc87530611b
Mark Scherer <euromark@web.de>
2015-04-25 13:50:19 +0200

    build

--- build.sh
+++ build.sh
@@ -8,18 +8,20 @@ git pull
 
 php composer.phar update --prefer-dist --no-dev --optimize-autoloader --no-interaction
 
+chmod +x bin/cake
+
+mkdir -p ./webroot/js/cjs/
+mkdir -p ./webroot/css/ccss/
+
+echo "### ASSETS ###";
 bin/cake AssetCompress.AssetCompress build
 
 echo "### DB MIGRATION ###";
-# Don't forget to "chmod +x bin/cake" first
 bin/cake Migrations migrate
 
 echo "### CLEANUP ###";
 rm -rf ./tmp/cache/models/*
 rm -rf ./tmp/cache/persistent/*
-
-mkdir -p ./webroot/js/cjs/
-mkdir -p ./webroot/css/ccss/
 
 chown -R www-data:www-data *
 chmod -R 0770 *
