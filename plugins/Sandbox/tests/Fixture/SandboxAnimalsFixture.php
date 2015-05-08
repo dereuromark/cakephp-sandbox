e2ec110942e51e5ef4aaf9825c6579808fe59d48
Mark Scherer <euromark@web.de>
2015-05-08 09:58:08 +0200

    fixture

--- plugins/Sandbox/tests/Fixture/SandboxAnimalsFixture.php
+++ plugins/Sandbox/tests/Fixture/SandboxAnimalsFixture.php
@@ -16,7 +16,7 @@ class SandboxAnimalsFixture extends TestFixture {
      */
     // @codingStandardsIgnoreStart
     public $fields = [
-        'id' => ['type' => 'integer', 'length' => 10, 'unsigned' => true, 'null' => false, 'autoIncrement' => true],
+        'id' => ['type' => 'integer', 'length' => 10, 'unsigned' => true],
         'name' => ['type' => 'string', 'length' => 100, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
         'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
