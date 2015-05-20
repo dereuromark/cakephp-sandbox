5bc862b823b4bceccff30be3e573c376bf41f38e
Mark Scherer <euromark@web.de>
2015-05-20 22:41:11 +0200

    Menu plugin

---
+++ plugins/Sandbox/src/Controller/MenuController.php
@@ -0,0 +1,34 @@
+<?php
+namespace Sandbox\Controller;
+
+use Cake\Event\Event;
+
+class MenuController extends SandboxAppController {
+
+	public $components = ['Gourmet/KnpMenu.Menu'];
+	public $helpers = ['Gourmet/KnpMenu.Menu'];
+
+	public function beforeFilter(Event $event) {
+		parent::beforeFilter($event);
+
+		$this->Auth->allow();
+	}
+
+	/**
+	 * PluginExamplesController::index()
+	 *
+	 * @return void
+	 */
+	public function index() {
+		$menu = $this->Menu->get('my_menu');
+
+		$menu->addChild('Dashboard',
+			['uri' => ['plugin' => false, 'controller' => 'Overview', 'action' => 'index']]);
+		$menu->addChild('Sandbox');
+		$menu->addChild('Coming soon');
+
+		$menu['Sandbox']->setUri('#sandbox');
+		$menu['Sandbox']->addChild('Overview', array('uri' => ['plugin' => 'Sandbox', 'controller' => 'Sandbox', 'action' => 'index']));
+	}
+
+}
---
+++ plugins/Sandbox/src/Template/Menu/index.ctp
@@ -0,0 +1,6 @@
+<h2>KnpMenu plugin</h2>
+
+<h3>Simple menu</h3>
+<?php
+	echo $this->Menu->render('my_menu');
+?>
