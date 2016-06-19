ca95ab3b68b355eb9bfa38c803ca81d2d44652be
Mark Scherer <euromark@web.de>
2016-06-19 14:29:11 +0200

    fix rating

---
+++ config/Migrations/20150602010204_rating_fix.php
@@ -0,0 +1,25 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Rating extends AbstractMigration {
+
+	/**
+	 * Migrate Up.
+	 */
+	public function up() {
+		$content = <<<SQL
+ALTER TABLE `sandbox_ratings` CHANGE `id` `id` INT(10) NOT NULL AUTO_INCREMENT;
+SQL;
+
+		$this->query($content);
+	}
+
+	/**
+	 * Migrate Down.
+	 */
+	public function down() {
+
+	}
+
+}
