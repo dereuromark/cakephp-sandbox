c989677ebc60aa36f22d5c6f19086d166e0f415d
mscherer <mark.scherer@spryker.com>
2017-01-12 12:25:14 +0100

    Add User entity.

---
+++ src/Model/Entity/User.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * User Entity
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $email
+ * @property string $password
+ * @property int $logins
+ * @property bool $active
+ * @property int $role_id
+ * @property \Cake\I18n\Time $created
+ * @property \Cake\I18n\Time $modified
+ * @property \Cake\I18n\Time $last_login
+ */
+class User extends Entity {
+
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        '*' => true,
+        'id' => false,
+    ];
+
+    /**
+     * Fields that are excluded from JSON versions of the entity.
+     *
+     * @var array
+     */
+    protected $_hidden = [
+        'password'
+    ];
+
+}
--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -3,6 +3,19 @@ namespace App\Model\Table;
 
 use Tools\Model\Table\Table;
 
+/**
+ * @property \Cake\ORM\Association\HasMany $SocialProfiles
+ *
+ * @method \App\Model\Entity\User get($primaryKey, $options = [])
+ * @method \App\Model\Entity\User newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\User[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\User|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\User patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\User[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\User findOrCreate($search, callable $callback = null)
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
 class UsersTable extends Table {
 
 	/**
@@ -49,6 +62,8 @@ class UsersTable extends Table {
 	 */
 	public function initialize(array $config) {
 		$this->belongsTo('Roles');
+
+		$this->displayField('username');
 
 		$this->addBehavior('Timestamp');
 	}
