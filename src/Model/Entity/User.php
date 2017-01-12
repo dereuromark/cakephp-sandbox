a4bba296d3f05415569efddfe0199567d57664da
dereuromark <euromark@web.de>
2017-01-13 00:28:20 +0100

    Fix CS

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -19,27 +19,27 @@ use Cake\ORM\Entity;
  */
 class User extends Entity {
 
-    /**
-     * Fields that can be mass assigned using newEntity() or patchEntity().
-     *
-     * Note that when '*' is set to true, this allows all unspecified fields to
-     * be mass assigned. For security purposes, it is advised to set '*' to false
-     * (or remove it), and explicitly make individual fields accessible as needed.
-     *
-     * @var array
-     */
-    protected $_accessible = [
-        '*' => true,
-        'id' => false,
-    ];
+	/**
+	 * Fields that can be mass assigned using newEntity() or patchEntity().
+	 *
+	 * Note that when '*' is set to true, this allows all unspecified fields to
+	 * be mass assigned. For security purposes, it is advised to set '*' to false
+	 * (or remove it), and explicitly make individual fields accessible as needed.
+	 *
+	 * @var array
+	 */
+	protected $_accessible = [
+		'*' => true,
+		'id' => false,
+	];
 
-    /**
-     * Fields that are excluded from JSON versions of the entity.
-     *
-     * @var array
-     */
-    protected $_hidden = [
-        'password'
-    ];
+	/**
+	 * Fields that are excluded from JSON versions of the entity.
+	 *
+	 * @var array
+	 */
+	protected $_hidden = [
+		'password'
+	];
 
 }
