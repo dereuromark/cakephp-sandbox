78d2760441eccea6b210f2946ac4306d0f9c8848
mscherer <mark.scherer@spryker.com>
2018-11-07 22:42:47 +0100

    Fix queue example.

--- config/app_custom.php
+++ config/app_custom.php
@@ -90,4 +90,19 @@ return [
 		'highlighter' => 'Markup\Highlighter\JsHighlighter'
 	],
 
+	'Queue' => [
+		'sleeptime' => 5,
+		'gcprob' => 10,
+		// time (in seconds) after which a job is requeued if the worker doesn't report back
+		'defaultworkertimeout' => 1800,
+		// number of retries if a job fails or times out.
+		'defaultworkerretries' => 2,
+		// seconds of running time after which the worker will terminate (0 = unlimited)
+		'workermaxruntime' => 120,
+		// instruct a Workerprocess quit when there are no more tasks for it to execute (true = exit, false = keep running)
+		'exitwhennothingtodo' => false,
+		// minimum time (in seconds) which a task remains in the database before being cleaned up.
+		'cleanuptimeout' => 2592000, // 30 days
+	],
+
 ];
