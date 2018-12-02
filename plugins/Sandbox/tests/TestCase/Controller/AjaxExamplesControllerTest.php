274d8dec6cdae41a19778caaa3a739dc7a7793c2
mscherer <mark.scherer@spryker.com>
2018-12-02 16:27:59 +0100

    CaePHP 3.7

--- plugins/Sandbox/tests/TestCase/Controller/AjaxExamplesControllerTest.php
+++ plugins/Sandbox/tests/TestCase/Controller/AjaxExamplesControllerTest.php
@@ -17,13 +17,6 @@ class AjaxExamplesControllerTest extends IntegrationTestCase {
 	public $fixtures = ['plugin.Data.Countries', 'app.users'];
 
 	/**
-	 * Allow router reloading to be disabled.
-	 *
-	 * @var bool
-	 */
-	protected $_disableRouterReload = true;
-
-	/**
 	 * @return void
 	 */
 	public function setUp() {
--- tests/TestCase/Controller/MiscControllerTest.php
+++ tests/TestCase/Controller/MiscControllerTest.php
@@ -5,8 +5,6 @@ namespace App\Test\TestCase\Controller;
 use Cake\ORM\TableRegistry;
 use Tools\TestSuite\IntegrationTestCase;
 
-/**
- */
 class MiscControllerTest extends IntegrationTestCase {
 
 	/**
