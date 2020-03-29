f69ab2b6cab4332ecb6dbf736ac84322ed8ab4b0
mscherer <mark.scherer@spryker.com>
2020-03-29 22:30:13 +0200

    Add tests

---
+++ plugins/Sandbox/tests/TestCase/Controller/FlashExamplesControllerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Sandbox\Test\TestCase\Controller;
+
+use Shim\TestSuite\IntegrationTestCase;
+
+/**
+ * @uses \Sandbox\Controller\FlashExamplesController
+ */
+class FlashExamplesControllerTest extends IntegrationTestCase {
+
+	/**
+	 * @return void
+	 */
+	public function testIndex() {
+		$this->get(['plugin' => 'Sandbox', 'controller' => 'FlashExamples', 'action' => 'index']);
+
+		$this->assertResponseCode(200);
+		$this->assertNoRedirect();
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testMessages() {
+		$this->disableErrorHandlerMiddleware();
+
+		$this->get(['plugin' => 'Sandbox', 'controller' => 'FlashExamples', 'action' => 'messages']);
+
+		$this->assertResponseCode(200);
+		$this->assertNoRedirect();
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testMessageGroups() {
+		$this->disableErrorHandlerMiddleware();
+
+		$this->get(['plugin' => 'Sandbox', 'controller' => 'FlashExamples', 'action' => 'messageGroups']);
+
+		$this->assertResponseCode(200);
+		$this->assertNoRedirect();
+	}
+
+}
