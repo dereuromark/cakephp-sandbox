d0973468a5ff12f757b3a8574623e958eb261dd4
mscherer <mark.scherer@spryker.com>
2021-11-12 11:34:26 +0100

    Upgrade routes config.

--- config/routes.php
+++ config/routes.php
@@ -2,7 +2,6 @@
 
 use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
 
 /**
  * The default class to use for all routes
@@ -11,42 +10,44 @@ use Cake\Routing\Router;
  * inconsistently cased URLs when used with `:plugin`, `:controller` and
  * `:action` markers.
  */
-Router::defaultRouteClass(DashedRoute::class);
+return static function (RouteBuilder $routes) {
+	$routes->setRouteClass(DashedRoute::class);
 
-Router::scope('/', function (RouteBuilder $routes) {
+	$routes->scope('/', function (RouteBuilder $routes) {
 
-	$routes->connect('/', ['controller' => 'Overview', 'action' => 'index']);
+		$routes->connect('/', ['controller' => 'Overview', 'action' => 'index']);
 
-	$routes->connect('/register', ['controller' => 'Account', 'action' => 'register']);
-	$routes->connect('/login', ['controller' => 'Account', 'action' => 'login']);
-	$routes->connect('/logout', ['controller' => 'Account', 'action' => 'logout']);
+		$routes->connect('/register', ['controller' => 'Account', 'action' => 'register']);
+		$routes->connect('/login', ['controller' => 'Account', 'action' => 'login']);
+		$routes->connect('/logout', ['controller' => 'Account', 'action' => 'logout']);
 
-	//route to switch locale
-	//$routes->connect('/lang/*', array('controller' => 'p28n', 'action' => 'change'));
+		//route to switch locale
+		//$routes->connect('/lang/*', array('controller' => 'p28n', 'action' => 'change'));
 
-	$routes->connect('/admin', ['prefix' => 'Admin', 'controller' => 'Overview', 'action' => 'index']);
+		$routes->connect('/admin', ['prefix' => 'Admin', 'controller' => 'Overview', 'action' => 'index']);
 
-	//$routes->connect('/translate', array('plugin' => 'Translate', 'controller' => 'TranslateGroups', 'action' => 'overview'));
+		//$routes->connect('/translate', array('plugin' => 'Translate', 'controller' => 'TranslateGroups', 'action' => 'overview'));
 
-	/**
-	 * ...and connect the rest of 'Pages' controller's URLs.
-	 */
-	$routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
+		/**
+		 * ...and connect the rest of 'Pages' controller's URLs.
+		 */
+		$routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
 
-	$routes->fallbacks();
-});
+		$routes->fallbacks();
+	});
 
-Router::prefix('Admin', function (RouteBuilder $routes) {
-	// Because you are in the admin scope,
-	// you do not need to include the /admin prefix
-	// or the admin route element.
-	$routes->fallbacks();
-});
+	$routes->prefix('Admin', function (RouteBuilder $routes) {
+		// Because you are in the admin scope,
+		// you do not need to include the /admin prefix
+		// or the admin route element.
+		$routes->fallbacks();
+	});
 
-// TMP
-Router::plugin('Data', function (RouteBuilder $routes) {
-	// Because you are in the admin scope,
-	// you do not need to include the /admin prefix
-	// or the admin route element.
-	$routes->fallbacks();
-});
+	// TMP
+	$routes->plugin('Data', function (RouteBuilder $routes) {
+		// Because you are in the admin scope,
+		// you do not need to include the /admin prefix
+		// or the admin route element.
+		$routes->fallbacks();
+	});
+};
