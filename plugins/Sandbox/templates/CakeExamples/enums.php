8a9bd6774ce735f34e126b2b62e909f81d5e5fc6
mscherer <dereuromark@web.de>
2023-11-03 15:50:14 +0100

    Fix up highlighting.

--- plugins/Sandbox/templates/CakeExamples/enums.php
+++ plugins/Sandbox/templates/CakeExamples/enums.php
@@ -25,7 +25,7 @@ With CakePHP 5 we can now use more native (backed) enums in our apps. They map t
 	We use a tinyint(2) column in the database for the `status` field.
 </p>
 
-<pre class="code-snippet"><?php echo h(file_get_contents(Plugin::path('Sandbox') . 'src/Model/Enum/UserStatus.php')); ?></pre>
+<?php echo $this->Highlighter->highlight(file_get_contents(Plugin::path('Sandbox') . 'src/Model/Enum/UserStatus.php'), ['lang' => 'php']); ?>
 
 <p>Now lets use `::cases()` to list all possible enum cases:</p>
 <ul>
@@ -38,26 +38,20 @@ With CakePHP 5 we can now use more native (backed) enums in our apps. They map t
 </ul>
 
 
-<?php if (!empty($result)) {
-	echo '<b>Result:</b>';
-	echo pre(h($result));
-}?>
-
 <h3>In DB and Forms</h3>
 <p>When used in forms, CakePHP form helper will automatically extract those to display as dropdown.</p>
 
 <p>For it to work, we need to assign the database table field to enum type:</p>
 
-<div><pre class="code-snippet"><?php
-	$text = <<<TXT
+<?php
+$text = <<<TXT
 // SandboxUsersTable
 public function initialize(array \$config): void {
     \$this->getSchema()->setColumnType('status', EnumType::from(UserStatus::class));
 }
 TXT;
-	echo h($text);
-	?></pre></div>
-
+echo $this->Highlighter->highlight($text, ['lang' => 'php']);
+?>
 
 <p>Let's load a demo record with the enum included now:</p>
 
@@ -65,9 +59,9 @@ TXT;
 <?php echo h($user->username); ?>
 </h4>
 
-<pre class="code-snippet"><?php
-	echo print_r($user, true);
-	?></pre>
+<?php
+echo $this->Highlighter->highlight(print_r($user, true), ['lang' => 'php']);
+?>
 
 <p>
 	Unfortunately, enums cannot implement Stringable.
@@ -93,10 +87,10 @@ When serializing, the actual (DB) value (in this case int) is used:
 
 <h4>json_encode() of the entity containing the enum</h4>
 
-<pre class="code-snippet"><?php
-	$text = json_encode($user, JSON_PRETTY_PRINT);
-	echo h($text);
-	?></pre>
+<?php
+$text = json_encode($user, JSON_PRETTY_PRINT);
+echo $this->Highlighter->highlight($text, ['lang' => 'json']);
+?>
 
 <p>
 If you also want the human-readable string form, you can add a virtual field `status_string` etc that would include this in the dataset.
@@ -104,11 +98,12 @@ If you also want the human-readable string form, you can add a virtual field `st
 
 <h4>Unserialize</h4>
 <p>json_decode() + patching an entity</p>
-<pre class="code-snippet"><?php
-	$array = json_decode($text, true);
-	$entity = \Cake\ORM\TableRegistry::getTableLocator()->get('Sandbox.SandboxUsers')->newEntity($array);
-	echo print_r($entity, true);
-	?></pre>
+
+<?php
+$array = json_decode($text, true);
+$entity = \Cake\ORM\TableRegistry::getTableLocator()->get('Sandbox.SandboxUsers')->newEntity($array);
+echo $this->Highlighter->highlight(print_r($entity, true), ['lang' => 'json']);
+?>
 
 <p>
 	Here you can see that it is now a backed enum object again.
