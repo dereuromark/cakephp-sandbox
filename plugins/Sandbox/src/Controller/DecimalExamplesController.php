8e043bf5a23742e5bd4ea771db3686f24166d675
mscherer <dereuromark@web.de>
2023-11-27 02:11:03 +0100

    Add decimal API examples.

--- plugins/Sandbox/src/Controller/DecimalExamplesController.php
+++ plugins/Sandbox/src/Controller/DecimalExamplesController.php
@@ -2,6 +2,9 @@
 
 namespace Sandbox\Controller;
 
+use PhpCollective\DecimalObject\Decimal;
+use Throwable;
+
 class DecimalExamplesController extends SandboxAppController {
 
 	/**
@@ -30,6 +33,54 @@ class DecimalExamplesController extends SandboxAppController {
 		}
 
 		$this->set(compact('record'));
+	}
+
+	/**
+	 * @return \Cake\Http\Response|null|void
+	 */
+	public function api() {
+		$operations = [
+			'add',
+			'subtract',
+			'multiply',
+			'divide',
+			'equals',
+			'greaterThan',
+			'lessThan',
+			'greaterThanOrEquals',
+			'lessThanOrEquals',
+			'pow',
+			'mod',
+		];
+
+		$result = null;
+		if ($this->request->is(['post', 'put'])) {
+			$one = $this->request->getData('one');
+			$two = $this->request->getData('two');
+			$operationKey = $this->request->getData('operation');
+			if (!isset($operations[$operationKey])) {
+				$this->Flash->error('This operation does not exist');
+
+				return $this->redirect([]);
+			}
+
+			$operation = $operations[$operationKey];
+			try {
+				$one = Decimal::create($one);
+				$two = Decimal::create($two);
+
+				if ($operation === 'divide') {
+					$result = $one->divide($two, max($one->scale(), $two->scale()));
+				} else {
+					$result = $one->$operation($two);
+				}
+
+			} catch (Throwable $exception) {
+				$this->Flash->error($exception->getMessage());
+			}
+		}
+
+		$this->set(compact('operations', 'result'));
 	}
 
 }
---
+++ plugins/Sandbox/templates/DecimalExamples/api.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var array<string> $operations
+ * @var mixed $result
+ */
+
+?>
+<?php $this->append('script'); ?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+<!-- and it's easy to individually load additional languages -->
+<script>hljs.highlightAll();</script>
+<?php $this->end(); ?>
+
+<nav class="actions col-sm-4 col-12">
+	<?php echo $this->element('navigation/decimal'); ?>
+</nav>
+<div class="page index col-sm-8 col-12">
+
+	<h3>Full API Demo</h3>
+
+	<p>Take value one and two and chose what operation to do on them.</p>
+
+	<?php echo $this->Form->create();?>
+	<fieldset>
+		<legend><?php echo __('Set values');?></legend>
+
+		<p>You can set integers or floating-point numbers.</p>
+
+		<?php
+		echo $this->Form->control('one', ['placeholder' => 'Format: 0 or 0.00']);
+		echo $this->Form->control('two', ['placeholder' => 'Format: 0 or 0.00']);
+		?>
+	</fieldset>
+	<fieldset>
+		<legend><?php echo __('Operation');?></legend>
+
+		<?php
+		echo $this->Form->control('operation', ['options' => $operations]);
+		?>
+	</fieldset>
+
+	<?php
+	echo $this->Form->submit(__('Submit'));
+	echo $this->Form->end();
+	?>
+
+	<h3>Result</h3>
+	<?php if ($result !== null) {
+		if ($result === false) {
+			$result = 'NO';
+		} elseif ($result === true) {
+			$result = 'YES';
+		}
+
+ 		echo $this->Highlighter->highlight(print_r($result, true), ['lang' => 'php']);
+	} ?>
+
+</div>
