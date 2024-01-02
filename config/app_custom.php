8cc56dabba69f5bd1127a38c00660a2457322044
mscherer <dereuromark@web.de>
2024-01-02 17:16:15 +0100

    Fix up tests

--- config/app_custom.php
+++ config/app_custom.php
@@ -319,7 +319,7 @@ $config = [
 
 if (str_contains(getenv('DB_URL'), 'mysql')) {
 	$config['Datasources']['default']['flags'][PDO::MYSQL_ATTR_INIT_COMMAND] = "SET sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''))";
-	$config['Datasources']['default']['test'][PDO::MYSQL_ATTR_INIT_COMMAND] = "SET sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''))";
+	$config['Datasources']['test']['flags'][PDO::MYSQL_ATTR_INIT_COMMAND] = "SET sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''))";
 }
 
 return $config;
