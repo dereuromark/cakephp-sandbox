9e23dac5e255e577a707efe0b7363c0498efe315
mscherer <euromark@web.de>
2024-09-12 13:50:24 +0200

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Warning: This is NOT a productive script, but for local dev envs only!
 
-set -o pipefail
+#set -o pipefail
 
-php composer.phar install
+COMPOSER_ALLOW_SUPERUSER=1 composer install
 
 [ ! -f config/app_local.php ] && cp config/app_local.default.php config/app_local.php && echo "ERROR: DB credentials missing, enter them now and run again!" && exit 1
 
@@ -11,6 +11,7 @@ mkdir -p ./tmp
 mkdir -p ./logs
 mkdir -p ./webroot/js/cjs/
 mkdir -p ./webroot/css/ccss/
+mkdir -p ./webroot/assets/
 
 chmod +x bin/cake
 
@@ -20,13 +21,13 @@ bin/cake maintenance_mode activate
 bin/cake queue worker end all
 
 echo "### DB MIGRATION ###";
-COMPOSER_ALLOW_SUPERUSER=1 php composer.phar migrate
+COMPOSER_ALLOW_SUPERUSER=1 composer migrate
 
 echo "### ASSETS ###";
 #npm install -g bower
 #npm install -g ttembed-js
 bower install
-COMPOSER_ALLOW_SUPERUSER=1 php composer.phar assets
+COMPOSER_ALLOW_SUPERUSER=1 composer assets
 
 ttembed-js ./webroot/assets/font-awesome/fonts/fontawesome-webfont.ttf
 
@@ -38,11 +39,11 @@ echo "### CLEANUP ###";
 rm -rf ./tmp/cache/models/*
 rm -rf ./tmp/cache/persistent/*
 
-bin/cake clear cache
+bin/cake cache clear_all
 bin/cake schema_cache build
 
 echo "### IDE HELPER ###";
-COMPOSER_ALLOW_SUPERUSER=1 php composer.phar setup
+COMPOSER_ALLOW_SUPERUSER=1 composer setup
 
 echo "### FINISH ###"
 bin/cake maintenance_mode deactivate
