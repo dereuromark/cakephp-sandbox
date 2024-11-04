9078d620d2d9c74b9d6c4215d4f988dd0a35edb9
mscherer <dereuromark@web.de>
2024-11-04 14:24:50 +0100

    Refactor plugin loading.

---
+++ config/plugins.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+	'Migrations' => ['onlyCli' => true],
+	'Shim' => [],
+	'Tools' => [],
+	'Setup' => [],
+	'Data' => [],
+	'Ajax' => [],
+	//'Meta', ['bootstrap' => false]);
+	'Cache' => [],
+	'AssetCompress' => [],
+	'TinyAuth' => ['bootstrap' => false],
+	'Calendar' => [],
+	'Search' => [],
+	'Ratings' => [],
+	'Comments' => [],
+	'Geo' => [],
+	'Templating' => [],
+	'DatabaseLog' => [],
+	'Queue' => [],
+	'Captcha' => [],
+	'CakeDto' => [],
+	'Cake/Localized' => [],
+	'BootstrapUI' => [],
+	'Markup' => [],
+	'Feedback' => [],
+	'Icings/Menu' => [],
+	'Expose' => [],
+	'Translate' => [],
+	'Favorites' => [],
+	'QrCode' => [],
+
+	// inside /plugins
+	'AuthSandbox' => [],
+	'Sandbox' => [],
+	'StateMachine' => [],
+	'StateMachineSandbox' => [],
+];
