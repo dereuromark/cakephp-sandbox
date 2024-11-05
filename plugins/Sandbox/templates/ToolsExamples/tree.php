a1c8ba66c4e1a450c61aeb04aa8081298f4d8c65
mscherer <dereuromark@web.de>
2024-11-05 20:49:25 +0100

    Tree examples.

--- plugins/Sandbox/templates/ToolsExamples/tree.php
+++ plugins/Sandbox/templates/ToolsExamples/tree.php
@@ -15,91 +15,99 @@ use Brick\VarExporter\VarExporter;
 </nav>
 <div class="page index col-sm-8 col-12">
 
-<h2>Tree structures - Tree behavior and helper</h2>
+	<h2>Tree structures - Tree behavior and helper</h2>
 
-<h3>Simple Example</h3>
-<p>Using `find(threaded)` on some demo categories.</p>
-<?php
+	<h3>Simple Example</h3>
+	<p>Using `find(threaded)` on some demo categories.</p>
+
+	<pre>echo $this->Tree->generate($tree);</pre>
+
+	<?php
 	echo $this->Tree->generate($tree);
-?>
+	?>
 
 
-<h3>Only generate 2 levels</h3>
+	<h3>Only generate 2 levels</h3>
 
-<?php
-echo $this->Tree->generate($tree, ['maxDepth' => 1]); // 0 based
-?>
+	<pre>echo $this->Tree->generate($tree, ['maxDepth' => 1]); // 0 based</pre>
 
-<h3>Using a callback to style</h3>
-<p>
-	And passing in "authPath" for the helper to know the currently active element and the path to it.
-</p>
-<?php
-// Helper function to find the depth of a tree branch
-function arrayDepth($row) {
-	$max_depth = 1;
-	$depth = 1;
+	<?php
+	echo $this->Tree->generate($tree, ['maxDepth' => 1]); // 0 based
+	?>
 
-	if ($row instanceof \Cake\ORM\Entity) {
-		$row = $row->toArray();
-	}
+	<h3>Using a callback to style</h3>
+	<p>
+		And passing in "authPath" for the helper to know the currently active element and the path to it.
+	</p>
 
-	if (!empty($row['children'])) {
-		foreach ($row['children'] as $child) {
-			$depth += arrayDepth($child);
+	<pre>echo $this->Tree->generate($tree, ['callback' => $callback, 'autoPath' => $autoPath]);</pre>
+	<p>(See code for details on the callback function).</p>
 
-			if ($depth > $max_depth) {
-				$max_depth = $depth;
-			}
-		}
-	}
+	<?php
+	// Helper function to find the depth of a tree branch
+	function arrayDepth($row) {
+		$max_depth = 1;
+		$depth = 1;
 
-	return $max_depth;
-}
-
-// Select the deepest record as active element for demo purposes
-function arrayPath($array, $dept = 0) {
-	$path = [];
-	$currentDepth = 0;
-
-	foreach ($array as $row) {
 		if ($row instanceof \Cake\ORM\Entity) {
 			$row = $row->toArray();
 		}
 
-		$deptOfPath = arrayDepth($row);
-		if ($deptOfPath < $currentDepth) {
-			continue;
-		}
-		$currentDepth = $deptOfPath;
-
-		$path = [$row['lft'], $row['rght']];
-
 		if (!empty($row['children'])) {
-			$path = arrayPath($row['children'], $dept + 1, $deptOfPath);
-			continue;
+			foreach ($row['children'] as $child) {
+				$depth += arrayDepth($child);
+
+				if ($depth > $max_depth) {
+					$max_depth = $depth;
+				}
+			}
 		}
+
+		return $max_depth;
 	}
 
-	return $path;
-}
+	// Select the deepest record as active element for demo purposes
+	function arrayPath(iterable $array, int $dept = 0) {
+		$path = [];
+		$currentDepth = 0;
 
-// Just for demo purposes [lft, right] is auto-selected
-$autoPath = arrayPath($tree);
+		foreach ($array as $row) {
+			if ($row instanceof \Cake\ORM\Entity) {
+				$row = $row->toArray();
+			}
 
-// Style using a callable
-$callback = function($params) {
-	$data = $params['data'];
-	$name = $data['name'];
+			$deptOfPath = arrayDepth($row);
+			if ($deptOfPath < $currentDepth) {
+				continue;
+			}
+			$currentDepth = $deptOfPath;
 
-	if ($params['activePathElement']) {
-		$name = '<b>' . $name . '</b>';
+			$path = [$row['lft'], $row['rght']];
+
+			if (!empty($row['children'])) {
+				$path = arrayPath($row['children'], $dept + 1);
+			}
+		}
+
+		return $path;
 	}
-	return $name;
-};
 
-echo $this->Tree->generate($tree, ['callback' => $callback, 'autoPath' => $autoPath]);
-?>
+	// Just for demo purposes [lft, right] is auto-selected
+	$autoPath = arrayPath($tree);
+
+	// Style using a callable
+	$callback = function($params) {
+		$data = $params['data'];
+		$name = $data['name'];
+
+		if ($params['activePathElement']) {
+			$name = '<b>' . $name . '</b>';
+		}
+		return $name;
+	};
+
+	echo $this->Tree->generate($tree, ['callback' => $callback, 'autoPath' => $autoPath]);
+	?>
 
 
 	<h3>Displaying a custom array</h3>
