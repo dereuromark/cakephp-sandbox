072fe92219542e5f7ea8d762682dd3cd1186cf63
Mark Scherer <dereuromark@users.noreply.github.com>
2025-11-10 23:58:55 +0100

    Update build script to use global composer command

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 # Warning: This is NOT a productive script, but for local dev envs only!
 
-echo "### INSTALL/UPDATE ###";
-[ ! -f composer.phar ] && curl -sS https://getcomposer.org/installer | php
-php composer.phar selfupdate
-
 git pull
 
-php composer.phar install --prefer-dist --no-dev --optimize-autoloader --no-interaction
+COMPOSER_ALLOW_SUPERUSER=1 composer install --prefer-dist --no-dev --optimize-autoloader --no-interaction
 
 chmod +x bin/cake
 
