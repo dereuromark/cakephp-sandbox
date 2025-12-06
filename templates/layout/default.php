8b92e3dee0bfbe1ef787d8db6f22e616e629b57f
mscherer <dereuromark@web.de>
2025-12-06 03:53:52 +0100

    Fix mobile rendering.

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -5,9 +5,10 @@
 $description = 'CakePHP Sandbox App';
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <?php echo $this->Html->charset(); ?>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>
 		<?php echo h($description); ?>:
 		<?php echo $this->fetch('title'); ?>
@@ -24,46 +25,36 @@ echo $this->fetch('css');
 </head>
 <body>
 
-	<?php
-	echo $this->element('navigation');
-	?>
+	<?php echo $this->element('navigation'); ?>
 
-	<div class="container">
+	<main class="container">
 
-		<div id="header">
-		</div>
-		<div id="content">
+		<?php echo $this->Flash->render(); ?>
 
-				<?php
-
-	echo $this->Flash->render();
-	?>
-
-				<div class="row">
-					<div class="col-12">
-				<?php
-					$content = $this->fetch('content');
-					if (preg_match('#^\s*<nav #', $content)) {
-						$content = '<div class="row">' . $content . '</div>';
-					}
-					echo $content;
-				?>
-					</div>
-				</div>
-
+		<div class="row">
+			<div class="col-12">
+			<?php
+				$content = $this->fetch('content');
+				if (preg_match('#^\s*<nav #', $content)) {
+					$content = '<div class="row">' . $content . '</div>';
+				}
+				echo $content;
+			?>
+			</div>
 		</div>
 
-			<hr />
+		<hr>
 
-		<div id="footer" class="">
+		<footer class="d-flex flex-wrap justify-content-between align-items-center py-2">
+			<div>
+				Author: dereuromark and <a href="https://github.com/dereuromark/cakephp-sandbox/graphs/contributors">contributors</a> |
+				Code: <a href="https://github.com/dereuromark/cakephp-sandbox">github.com/dereuromark/cakephp-sandbox</a> |
+				<?php echo $this->Html->linkReset('Contact', ['controller' => 'Contact', 'action' => 'index']); ?>
+			</div>
+			<div class="text-muted">Running on CakePHP <?php echo $this->Configure->version(); ?> / PHP <?php echo substr(phpversion(), 0, 3); ?></div>
+		</footer>
 
-			<div style="float: right;">Running on CakePHP <?php echo $this->Configure->version(); ?> / PHP <?php echo substr(phpversion(), 0, 3);?></div>
-
-				Author: dereuromark and <a href="https://github.com/dereuromark/cakephp-sandbox/graphs/contributors">contributors</a> | Code: <a href="https://github.com/dereuromark/cakephp-sandbox">github.com/dereuromark/cakephp-sandbox</a> | <?php
-	echo $this->Html->linkReset('Contact', ['controller' => 'Contact', 'action' => 'index']);
-	?>
-		</div>
-	</div>
+	</main>
 
 <?php if (!$this->Configure->read('debug')) {
 	echo $this->element('stats');
