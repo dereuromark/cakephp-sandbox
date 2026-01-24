bbfb48be87d9c0dc778947fcafd5a72cbe5c6ef8
Mark Scherer <dereuromark@users.noreply.github.com>
2026-01-24 17:50:10 +0700

    Remove duplicate MiscController::dtoProjection (use Sandbox.DtoExamples instead). (#107)

--- tests/TestCase/Controller/MiscControllerTest.php
+++ tests/TestCase/Controller/MiscControllerTest.php
@@ -35,18 +35,4 @@ class MiscControllerTest extends IntegrationTestCase {
 		$this->assertNoRedirect();
 	}
 
-	/**
-	 * Test dtoProjection action
-	 *
-	 * @return void
-	 */
-	public function testDtoProjection(): void {
-		$this->disableErrorHandlerMiddleware();
-
-		$this->get(['controller' => 'Misc', 'action' => 'dtoProjection']);
-
-		$this->assertResponseCode(200);
-		$this->assertNoRedirect();
-	}
-
 }
